e3ffd2b13e01de730afff80ed0741f6382a5c052
silsha fux <hallo@silsha.me>
2015-11-06 22:00:01 +0100

    mqtt2influx: increase readability

--- mqtt2influx/index.js
+++ mqtt2influx/index.js
@@ -3,6 +3,7 @@ var client  = mqtt.connect('mqtt://infra.rzl');
 var influx  = require('influx');
 var config  = require('./config.json');
 
+
 // Influx client configuration
 var influx = influx({ 
   host : 'localhost',
@@ -11,19 +12,26 @@ var influx = influx({
   database : config.database
 })
 
+
 // Subscribe to all topics
 client.subscribe('#');
+
 
 // Handle new messages
 client.on('message', function (topic, message) {
   message = JSON.parse(message);
+  
   if (message.state !== undefined) {
+
     influx.writePoint(topic, message.state, {time: message.when}, function(err, response) {
       if (err) { console.log(err); }
     });
+
   } else {
+
     influx.writePoint(topic, message, {}, function(err, response) {
       if (err) { console.log(err); }
     });
+
   }
 });
